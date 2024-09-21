ce8a8aa2ca6fcb9878f9cb98e6c9bebb7e1d41f0
Nishihara Tsubasa <95861046+Crayfisher-zari@users.noreply.github.com>
2024-09-21 22:19:04 +0900

    出力パスの修正

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 const basePath = process.env.BASE_PATH || "/";
 export default defineConfig({
   build: {
-    outDir: "../dist",
+    outDir: "./dist",
     base: basePath,
   },
 });
